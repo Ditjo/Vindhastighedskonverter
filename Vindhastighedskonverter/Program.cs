d5b2afb9b12e82c3096e381a4935b16df434e478
Ditjo <mads933c@elev.tec.dk>
2022-05-02 12:27:25 +0200

    Lot's of changes all over the place, but looks the same.

--- Vindhastighedskonverter/Program.cs
+++ Vindhastighedskonverter/Program.cs
@@ -5,101 +5,64 @@ namespace Vindhastighedskonverter
     {
         static void Main(string[] args)
         {
-            string menu1 = "0", menu2 = "0", valg1, valg2;
+            string Speedstr;
+            int menu1 = 0, menu2 = 0;
             double SpeedIn, SpeedMid, SpeedOut = 0;
+            bool result;
+            string[] id = { "m/s", "km/h", "mph", "knots" };
 
-            Console.WriteLine("Vindhastighedskonverter");
-            Console.WriteLine("Hvilken enhed vil du konvertere fra?");
-            Menu(menu1);
-            menu1 = Console.ReadLine();
-
-            Console.Write("Hvad er hastigheden:");
-            SpeedIn = Convert.ToDouble(Console.ReadLine());
-
-            SpeedMid = ConvKM(SpeedIn, menu1);
-
-            Console.WriteLine("Hvilken enhed vil du konvertere til?");
-            Menu(menu1);
-            menu2 = Console.ReadLine();
-
-            SpeedOut = ConvResult(SpeedMid, menu2);
-
-            Console.WriteLine($"Hastigheden er {SpeedOut}");
-        }
-
-        static internal void Menu(string menu1)
-        {
-            if (menu1 != "1")
+            //Convert from Input
+            do
             {
-                Console.WriteLine("1. m/s");
-            }
-            if (menu1 != "2")
+                Console.WriteLine("Vindhastighedskonverter");
+                Console.WriteLine("Hvilken enhed vil du konvertere fra?");
+                //calls Tools.Menu to create menu
+                Tools.Menu(menu1);
+                
+                //Tryparse the input. If fail, write error message.
+                result = Int32.TryParse(Tools.GetInput(), out menu1);
+                if (result == false)
+                    Console.WriteLine("Indsæt gyldig input");
+
+                //checks if the numbers are valid.
+                Tools.CorrectInput(menu1);
+
+            } while (menu1 != 1 && menu1 != 2 && menu1 != 3 && menu1 != 4);
+
+            //Speed Input
+            do
             {
-                Console.WriteLine("2. km/h");
-            }
-            if (menu1 != "3")
+                Console.Write("Hvad er hastigheden: ");
+                Speedstr = Tools.GetInput();
+                //tryparse to double. If fail write error message
+                result = double.TryParse(Speedstr, out SpeedIn);
+                if (result == false)
+                    Console.WriteLine("Indsæt gyldig input");
+            } while (result == false);
+
+            //Convert from input to km/h
+            SpeedMid = Tools.ConvKM(SpeedIn, menu1);
+
+            //Convert to Input
+            do
             {
-                Console.WriteLine("3. mph");
-            }
-            if (menu1 != "4")
-            {
-                Console.WriteLine("4. knots");
-            }
-        }
-        static internal double ConvKM(double speed, string menu1)
-        {
-            switch (menu1)
-            {
-                case "1":
-                    {
-                        return speed * 3.6;
-                    }
-                case "2":
-                    {
-                        return speed;
-                    }
-                case "3":
-                    {
-                        return speed * 1.6;
-                    }
-                case "4":
-                    {
-                        return speed * 1.8;
-                    }
-                default:
-                    {
-                        
-                        Console.WriteLine("Fejl");
-                        return speed = 0;
-                    }
-            }
-        }
-        static internal double ConvResult(double speed, string menu2)
-        {
-            switch (menu2)
-            {
-                case "1":
-                    {
-                        return speed / 3.6;
-                    }
-                case "2":
-                    {
-                        return speed;
-                    }
-                case "3":
-                    {
-                        return speed / 1.6;
-                    }
-                case "4":
-                    {
-                        return speed / 1.8;
-                    }
-                default:
-                    {
-                        Console.WriteLine("Fejl");
-                        return 0;
-                    }
-            }
+                Console.WriteLine("Hvilken enhed vil du konvertere til?");
+                //calls Tools.Menu to create menu
+                Tools.Menu(menu1);
+
+                //Tryparse the input. If fail, write error message.
+                result = Int32.TryParse(Tools.GetInput(), out menu2);
+                if (result == false)
+                    Console.WriteLine("Indsæt gyldig input");
+
+                //checks if the numbers are valid.
+                Tools.CorrectInput(menu2);
+            } while (menu2 != 1 && menu2 != 2 && menu2 != 3 && menu2 != 4);
+
+            //converts from km/h to final output
+            SpeedOut = Tools.ConvResult(SpeedMid, menu2);
+
+            Console.WriteLine($"Hastigheden er {SpeedOut} {id[menu2 - 1]}");
         }
     }
 }
---
+++ Vindhastighedskonverter/Tools.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vindhastighedskonverter
+{
+    internal class Tools
+    {
+        //Get's input & makes sure it's not empty or null
+        static internal string GetInput()
+        {
+            string? input;
+            do
+            {
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    Console.WriteLine("Indsæt gyldig input");
+            } while (string.IsNullOrEmpty(input));
+            return input;
+        }
+        //Se if input is a valid number
+        static internal void CorrectInput(int input)
+        {
+            if (input != 1 && input != 2 && input != 3 && input != 4)
+            {
+                Console.WriteLine("Indsæt gyldig input\n");
+            }
+        }
+        //creates the menu. You are not able to convert from one thing to the same thing.
+        static internal void Menu(int menu1)
+        {
+
+            if (menu1 != 1)
+            {
+                Console.WriteLine($"1. m/s");
+            }
+
+            if (menu1 != 2)
+            {
+                Console.WriteLine($"2. km/h");
+            }
+
+            if (menu1 != 3)
+            {
+                Console.WriteLine($"3. mph");
+            }
+
+            if (menu1 != 4)
+            {
+                Console.WriteLine($"4. knots");
+            }
+        }
+        //Converts from input to km/h
+        static internal double ConvKM(double speed, int menu1)
+        {
+            switch (menu1)
+            {
+                case 1:
+                    {
+                        return speed * 3.6;
+                    }
+                case 2:
+                    {
+                        return speed;
+                    }
+                case 3:
+                    {
+                        return speed * 1.6;
+                    }
+                case 4:
+                    {
+                        return speed * 1.8;
+                    }
+                default:
+                    {
+
+                        Console.WriteLine("Fejl");
+                        return speed = 0;
+                    }
+            }
+        }
+        //Converts from km/h to output
+        static internal double ConvResult(double speed, int menu2)
+        {
+            switch (menu2)
+            {
+                case 1:
+                    {
+                        return speed / 3.6;
+                    }
+                case 2:
+                    {
+                        return speed;
+                    }
+                case 3:
+                    {
+                        return speed / 1.6;
+                    }
+                case 4:
+                    {
+                        return speed / 1.8;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Fejl");
+                        return 0;
+                    }
+            }
+        }
+    }
+}
